c9476beb999c3e33c7059dd061ecb14c05b4410c
Radheykant <nocamin0007@gmail.com>
2024-08-31 15:07:57 +0000

    update project id

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "advance-terraform-434208" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
